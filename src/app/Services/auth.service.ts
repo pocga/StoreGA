c4c13052f010a850a1c512397ed61b9e18d22ed7
Leonardo Cadavid <lcadavidz@yahoo.com>
2019-04-09 10:24:49 -0500

    Conexión

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -21,21 +21,12 @@ export class AuthService {
 
         /*// Producción
           ClientId : '4l482gmr6vl5vs55k8hh4t0ol2', // Your client id here
-         // ClientId : 'iq5d9op5dpme4mqgsogedr56h', // Your client id here
-
           AppWebDomain : 'tech-shop.auth.us-east-1.amazoncognito.com',
-         // AppWebDomain : 'tech-shop-dev.auth.us-east-1.amazoncognito.com',
-
           TokenScopesArray : ['phone', 'email', 'profile','openid', 'aws.cognito.signin.user.admin'], // e.g.['phone', 'email', 'profile','openid', 'aws.cognito.signin.user.admin'],
           RedirectUriSignIn : 'https://www.techshopga.com/managetoken',
           RedirectUriSignOut : 'https://www.techshopga.com/signout',
-
-         // RedirectUriSignIn : 'https://localhost:4200/managetoken',
-         // RedirectUriSignOut : 'https://localhost:4200/signout',
           IdentityProvider : '', // e.g. 'Facebook',
-
           UserPoolId : 'us-east-1_CVePKGhTu', // Your user pool id here
-          //UserPoolId : 'us-east-1_JpBGocsnE', // Your user pool id here
           AdvancedSecurityDataCollectionFlag : false, // e.g. true
           Storage: '' // OPTIONAL e.g. new CookieStorage(), to use the specified storage provided
         */
